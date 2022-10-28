90889894bda2d1725a6018620fb47e676b36ccd0
dogatuncer <dogatuncer00@gmail.com>
2022-10-28 21:41:57 +0300

    Product Use case created. Updated repository and Implementation.

--- data/repository/CapybaraRepositoryImpl.kt
+++ data/repository/CapybaraRepositoryImpl.kt
@@ -1,13 +1,52 @@
 package com.example.capybara.data.repository
 
+import com.example.capybara.data.models.CartProduct
+import com.example.capybara.data.models.Product
 import com.example.capybara.data.repository.local.LocalDataSource
 import com.example.capybara.data.repository.remote.RemoteDataSource
 import com.example.capybara.domain.repository.CapybaraRepositoryInterface
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
 import javax.inject.Inject
 
 class CapybaraRepositoryImpl @Inject constructor(
     private val remoteDataSource: RemoteDataSource,
     private val localDataSource: LocalDataSource
 ) : CapybaraRepositoryInterface {
+    override suspend fun getAllProducts(): Response<List<Product>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getProduct(ItemId: Int): Response<Product> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getAllCategories(): Response<List<String>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getCategoryProducts(category: String): Response<List<Product>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addToCart(product: CartProduct) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCartItems(): Flow<List<CartProduct>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateCartItems(product: CartProduct) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteCartItems(product: CartProduct) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun clearCart() {
+        TODO("Not yet implemented")
+    }
 
 }
--- domain/repository/CapybaraRepositoryInterface.kt
+++ domain/repository/CapybaraRepositoryInterface.kt
@@ -1,4 +1,20 @@
 package com.example.capybara.domain.repository
 
+import com.example.capybara.data.models.CartProduct
+import com.example.capybara.data.models.Product
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
+
 interface CapybaraRepositoryInterface {
+    suspend fun getAllProducts() : Response<List<Product>>
+    suspend fun getProduct(ItemId : Int) : Response<Product>
+    suspend fun getAllCategories() : Response<List<String>>
+    suspend fun getCategoryProducts(category : String) : Response<List<Product>>
+
+    suspend fun addToCart(product: CartProduct)
+    fun getCartItems() : Flow<List<CartProduct>>
+    suspend fun updateCartItems(product: CartProduct)
+    suspend fun deleteCartItems(product: CartProduct)
+    suspend fun clearCart()
+
 }
